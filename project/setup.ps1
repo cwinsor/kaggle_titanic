f836faea3919877db5b1b99df45dcf743490252a
Chris Winsor <cwinsor@gmail.com>
2019-07-30 21:38:06 -0400

    updating setup files

---
+++ project/setup.ps1
@@ -0,0 +1,15 @@
+
+$TB_ROOT    = $PSScriptRoot
+$TB_SCRIPTS = $PSScriptRoot
+
+# if the virtualenv (pymote_env) does not exist then create it
+# required libraries are identified in requirements.txt
+
+if (-not (Test-Path "$TB_ROOT\pymote_env\Scripts\activate" -PathType Leaf)) {
+    &"$TB_SCRIPTS\setup_onetime_00_restore_libraries.ps1"
+    }
+
+.\setup_everytime_00_activate_env.ps1
+.\setup_everytime_01_add_cwd_to_path.ps1
+.\setup_everytime_02_start_jupyter_notebook
+#.\scripts\setup_everytime_03_start_code.ps1
--- project/setup_everytime_00_activate_env.ps1
+++ project/setup_everytime_00_activate_env.ps1
@@ -1,3 +1,5 @@
+cd $TB_ROOT
+
 # activate the virtual environment
 # if the pymote_env does not exist - give user a good message
 if (Test-Path '.\pymote_env\Scripts\activate' -PathType Leaf) {
@@ -5,7 +7,5 @@ if (Test-Path '.\pymote_env\Scripts\activate' -PathType Leaf) {
     } else {
     "did not find pymote_env\Scripts\activate - do you need to run setup_onetime?"
     }
-
-
 
 
