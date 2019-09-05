0d0e6bd23da5812ad1d1e36f5c7699b5bbc470c5
Chris Winsor <cwinsor@gmail.com>
2019-09-05 04:41:22 -0400

    add NB2-Decision Trees

--- project/setup_everytime_01_add_cwd_to_path.ps1
+++ project/setup_everytime_01_add_cwd_to_path.ps1
@@ -1,9 +1,8 @@
 
 # add current directory to PATH (so libraries can be found)
+
 $Env:PYTHONPATH=''
+$Env:PYTHONPATH="$ENV:PYTHONPATH/$PSScriptRoot;"
+$Env:PYTHONPATH="$ENV:PYTHONPATH/$PSScriptRoot/lib;"
+$Env:PYTHONPATH="$ENV:PYTHONPATH/$PSScriptRoot/lib/python-id3-trees;"
 
-$Env:PYTHONPATH=$ENV:PYTHONPATH + $PSScriptRoot
-$Env:PYTHONPATH=$ENV:PYTHONPATH + ';'
-
-$Env:PYTHONPATH=$ENV:PYTHONPATH + $PSScriptRoot
-$Env:PYTHONPATH=$ENV:PYTHONPATH + '/lib;'
