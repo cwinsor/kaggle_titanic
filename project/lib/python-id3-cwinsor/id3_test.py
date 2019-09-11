307d6b787b1c02073cb12cb35fb881ca4452ba71
Chris Winsor <cwinsor@gmail.com>
2019-09-10 23:10:29 -0400

    updates to lib/cwinsor_id3 which attempts to implement id3 from pseudocode

--- project/lib/python-id3-cwinsor/id3_test.py
+++ project/lib/python-id3-cwinsor/id3_test.py
@@ -5,36 +5,25 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.tree import DecisionTreeClassifier
 from id3_winsor_file import ID3_Class
 
-data = pd.DataFrame(np.array([['red', 'round', 'apple'],
-                              ['yellow', 'long', 'banana'],
-                              ['orange', 'round', 'orange']]),
-             columns=['Color', 'Shape', 'Type'])
+data = pd.DataFrame(np.array([
+    ["Sunny",    "Hot",  "High",   "Weak",   "No" ],
+    ["Sunny",    "Hot",  "High",   "Strong", "No" ],
+    ["Overcast", "Hot",  "High",   "Weak",   "Yes"],
+    ["Rain",     "Mild", "High",   "Weak",   "Yes"],
+    ["Rain",     "Cool", "Normal", "Weak",   "Yes"],
+    ["Rain",     "Cool", "Normal", "Strong", "No" ],
+    ["Overcast", "Cool", "Normal", "Strong", "Yes"],
+    ["Sunny",    "Mild", "High",   "Weak",   "No" ],
+    ["Sunny",    "Cool", "Normal", "Weak",   "Yes"],
+    ["Rain",     "Mild", "Normal", "Weak",   "Yes"],
+    ["Sunny",    "Mild", "Normal", "Strong", "Yes"],
+    ["Overcast", "Mild", "High",   "Strong", "Yes"],
+    ["Overcast", "Hot",  "Normal", "Weak",   "Yes"],
+    ["Rain",     "Mild", "High",   "Strong", "No" ]]),
+             columns=['Outlook', 'Temperature', 'Humidity', 'Wind', 'PlayTennis'])
 
-print(data)
-
-print(data.columns[0])
-
-#        argv = sys.argv
-#        print("Command line args are {}: ".format(argv))
-#
-#        config = load_config(argv[1])
-#
-#        data = load_csv_to_header_data(config['data_file'])
-#        data = project_columns(data, config['data_project_columns'])
-#
-#        target_attribute = config['target_attribute']
-#       remaining_attributes = set(data['header'])
-#       remaining_attributes.remove(target_attribute)
-#
-#        uniqs = get_uniq_values(data)
-#
-target_attribute = "Type"
-attributes = ['Color', 'Shape']
+attributes = ['Humidity', 'Wind', 'Outlook']
+target_attribute = "PlayTennis"
 
 id3_instance = ID3_class()
-id3_kinstance.fit(data, target_attribute, attributes)
-
-root = id3(data, uniqs, remaining_attributes, target_attribute)
-#
-#        pretty_print_tree(root)
-
+id3_instance.fit(data, target_attribute, attributes)
